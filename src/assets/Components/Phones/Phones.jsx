5a1d9f521c43cc18c677059b828780bc3770bcd4
abujaher5 <abujaher5120@gmail.com>
2024-03-21 17:26:39 +0600

    added loder from awesome components

--- src/assets/Components/Phones/Phones.jsx
+++ src/assets/Components/Phones/Phones.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 import { useEffect, useState } from "react";
 import {
   Bar,
@@ -12,6 +13,8 @@ import {
 
 const Phones = () => {
   const [phones, setPhones] = useState([]);
+
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     // fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
     //   .then((res) => res.json())
@@ -28,13 +31,26 @@ const Phones = () => {
           };
           return obj;
         });
+
+        // console.log(phonesWithFakeData);
         setPhones(phonesWithFakeData);
-        console.log(phonesWithFakeData);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div>
+      {loading && (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
+      )}
       <h3 className="text-4xl text-center">Phones:{phones.length}</h3>
 
       <BarChart width={1200} height={400} data={phones}>
@@ -46,6 +62,10 @@ const Phones = () => {
         <Legend />
         <Tooltip />
       </BarChart>
+
+      <div>
+        <h3 className="text-4xl text-center">Day Picker Calender</h3>
+      </div>
     </div>
   );
 };
